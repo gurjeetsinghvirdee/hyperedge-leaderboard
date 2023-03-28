b9429eb62b9b20fd393a97e2f37e02864159ee93
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-03-29 04:25:07 +0530

    working with searchbar for searching users in leaderboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -18,7 +18,9 @@ import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import AppBar  from '@material-ui/core/AppBar'; 
 import Confetti from 'react-confetti';
-import { Spinner } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SkeletonCircle, SkeletonText, Spinner } from '@chakra-ui/react';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const columns = [
     { id: 'position', label: 'Position', minWidth: 50, align: 'center' },
@@ -122,18 +124,55 @@ const useStyles = makeStyles((theme) => {
     }
 });
 
+function useWindowDimensions() {
+    const hasWindow = typeof window !== "undefined";
+
+    var getWindowDimensions = useCallback(() => {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+            width,
+            height
+        };
+    }, [hasWindow]);
+
+    const [windowDimesnions, setWindowDimensions] = useState(
+        getWindowDimensions()
+    );
+
+    useEffect(() => {
+        if (hasWindow) {
+            function handleResize() {
+                setWindowDimensions(getWindowDimensions());
+            }
+
+            window.addEventListener("resize", handleResize);
+            return () => window.removeEventListener("resize", handleResize)
+        }
+    }, [getWindowDimensions, hasWindow]);
+
+    return windowDimesnions;
+}
+
+
 function Leaderboard() {
     const classes = useStyles();
-    let [leaderss, setLeaderss] = useState({});
+    let [totalData, setTotalData] = useState([]);
+    let [leaderss, setLeaderss] = useState([]);
+    let [searchData, setSearchData] = useState([]);
     let [links, setLinks] = useState("");
     let [login, setLogin] = useState("");
     let [score, setScore] = useState("");
     let [avatar, setAvatar] = useState("");
     let [isLoading, setIsLoading] = useState("");
-    let [loadingMsg, setLoadingMsg] = useState("");
+    let [isLboardLoading, setIsLboardLoading] = useState("");
+    let [loadingMsg, setLoadingMsg] = useState("Request sended to the server");
+    let [filter, setFilter] = useState("");
     let [showConfetti, setShowConfetti] = useState("");
     let [lastupdated, setLastupdated] = useState("");
     const [openn, setOpenn] = React.useState(true);
+    const [activePage, setActivePage] = useState(1);
+    const { height, width } = useWindowDimensions();
     let rows = [];
     function createData(username, avatar, prnums, score, prlinks, level0, level1, level2, level3, level4) {
         return { username, avatar, prnums, score, prlinks, level0, level1, level2, level3, level4 };
@@ -166,6 +205,22 @@ function Leaderboard() {
         setAvatar(leaderss[num].avatar_url);
         setScore(leaderss[num].score);
     };
+
+    const filterData = () => {
+        setIsLboardLoading(true);
+        if (filter === "" && leaderss.length !== totalData.length) {
+          setSearchData(totalData);
+          setActivePage(1);
+          setIsLboardLoading(false);
+        } else {
+          const filtered = totalData.filter((leader) =>
+            leader.login.toLowerCase().includes(filter.toLowerCase())
+          );
+          setSearchData(filtered);
+          setActivePage(1);
+          setIsLboardLoading(false);
+        }
+      };
 
     const handleClose = () => {
         prlinks = []
@@ -313,7 +368,152 @@ function Leaderboard() {
                         <span className='text-primary_red-0' style={{ display: "block" }}> HyperEdge WOB 2023 Leaderboard</span>
                         Top Performers
                     </div>
-                    
+                </div>
+            </div>
+            <div>
+                <div className='py-5 px-0 xl:pb-12 xl:px-24 xl:pt-0 text-center'>
+                    <div className='flex md:flex-row justify-between gap-y-1 gap-x-1 md:gap-x-2 items-center my-10'>
+                        <div className='bg-white shadow-2xl dark:bg-black rounded-md px-0 sm:px-3 py-2 md:px-16 lg:py-4 relative inline-block w-28 md:w-auto'>
+                            {totalData[1] === undefined && (
+                                <>
+                                    <SkeletonCircle className='skeleton-circle-md' />
+                                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                                </>
+                            )}
+                            {totalData[1] !== undefined && (
+                                <>
+                                    <img alt='avatar img'
+                                        className='w-12 md:w-16 lg:w-24 rounded-full m-auto inline-block object-cover bg-white'
+                                        src={
+                                            totalData[1] !== undefined
+                                                ? totalData[1].avatar_url
+                                                : null
+                                        }
+                                    />
+                                    <FontAwesomeIcon
+                                        className="invisible lg:visible w-8 h-8 rounded-full bottom-5 border-white absolute bottom-1/4 right-1/4 bg-red-300 inline-block"
+                                        icon={faGithub}
+                                        size='2x'
+                                    />
+                                    <h3 className='text-black dark:text-primary_red-0 font-semibold mt-2 text-xs sm:text-sm md:text-md'>
+                                        2.{" "}
+                                        {totalData[2] !== undefined ? totalData[2].login : null}
+                                    </h3>
+                                </>
+                            )}
+                        </div>
+                        <div className='bg-white shadow-2xl dark:bg-black rounded-md px-0 sm:px-3 py-2 md:px-16 lg:py-4  relative inline-block w-28 md:w-auto'>
+                            {totalData[0] === undefined && (
+                                <>
+                                    <SkeletonCircle className='skeleton-circle-lg' />
+                                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                                </>
+                            )}
+                            {totalData[0] === undefined && (
+                                <>
+                                    <img alt='avatar'
+                                        className='w-12 md:w-16 lg:w-40 rounded-full m-auto bg-white'
+                                        src={
+                                            totalData[0] !== undefined
+                                                ? totalData[0].avatar_url
+                                                : null
+                                        }
+                                    />
+                                    <FontAwesomeIcon 
+                                        className='invisible lg:visible w-10 h-10 rounded-full border-5 border-white absolute bottom-1/4 right-1/4 bg-cyan-200 inline-block'
+                                        icon={faGithub}
+                                        size='3x'
+                                    />
+                                    <h3 className='text-black dark:text-primary_orange-0 font-semibold mt-4 text-xs sm:text-sm md:text-md'>
+                                        1.{" "}
+                                        {totalData[0] !== undefined ? totalData[0].login : null}
+                                    </h3>
+                                </>
+                            )}
+                        </div>
+
+                        <div className="bg-white shadow-2xl dark:bg-black rounded-md px-0 sm:px-3 py-2 md:px-16 lg:py-4 relative inline-block w-28 md:w-auto">
+                            {totalData[2] === undefined && (
+                                <>
+                                    <SkeletonCircle className="skeleton-circle-md" />
+                                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                                </>
+                        )}
+                            {totalData[2] !== undefined && (
+                                <>
+                                    <img alt='avatar'
+                                    className="w-12 md:w-16 lg:w-24 rounded-full m-auto bg-white"
+                                    src={
+                                        totalData[2] !== undefined
+                                        ? totalData[2].avatar_url
+                                        : null
+                                    }
+                                />
+                                <FontAwesomeIcon
+                                className="invisible lg:visible w-8 h-8 rounded-full border-5 border-white absolute bottom-1/4 right-1/4 bg-zinc-100 inline-block"
+                                icon={faGithub}
+                                size="2x"
+                                />
+                                <h3 className="text-black dark:text-primary_orange-0 font-semibold mt-2 text-xs sm:text-sm md:text-md">
+                                    3.{" "}
+                                    {totalData[2] !== undefined? totalData[2].login : null}
+                                </h3>
+                            </>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className='mt-20'>
+                        <div className='flex justify-end'>
+                            <div className='mb-3 xl:w-96'>
+                                <div className='input-group relative flex flex-wrap items-stretch w-full mb-4 justify-end'>
+                                    <div className='relative flex search-container'>
+                                        <input
+                                            onChange={(e) => {
+                                                setFilter(e.target.value);
+                                            }}
+                                            type='search'
+                                            className='form-control relative flex flex-auto min-w-0 block px-3 py-1.5 text-base dark:bg-neutral-800 dark:text-neutral-100 dark:border-neutral-600 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-gray-400 focus:outline-none dark:placeholder-neutral-300'
+                                            placeholder='Search'
+                                            aria-label='Search'
+                                            aria-describedby='button-addon2'
+                                            onKeyUp={(e) => {
+                                                e.key = "Enter" ? filterData() : "";
+                                            }}
+                                         />
+                                         <span className='search-count dark:text-neutral-300'>
+                                            {searchData.length}
+                                         </span>
+                                    </div>
+                                    <button
+                                        onClick={() => {
+                                            filterData();
+                                        }}
+                                        className='btn inline-block px-6 py-2.5 bg-gray-300 dark:bg-neutral-600 text-gray-600 font-medium text-xs leading-tight uppercase hover:text-gray-800 focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex items-center'
+                                        type='button'
+                                        id='button-addon2'
+                                        style={{
+                                            padding: "10px 18px",
+                                            maxWidth: "50px",
+                                            width: "20%",
+                                        }}
+                                    >
+                                        <svg className='w-4 fill-neutral-600 hover:fill-neutral-800 dark:fill-neutral-300 dark:hover:fill-neutral-100'
+                                            aria-hidden="true"
+                                            focusable="false"
+                                            data-prefix="fas"
+                                            data-icon="search"
+                                            role='img'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            viewBox='0 0 512 512'                                        
+                                        >
+                                            <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
           </div>
